167ca3b3263b444a624bc5d249f0134aae3f7dbb
miracarslan23 <aralan2000@icloud.com>
2025-03-16 23:12:59 +0300

    Update ex004.py

--- Exercises/ex004.py
+++ Exercises/ex004.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:210543008
+# Your Name and Surname:mustafa miraç arslan
+r = float(input("dairenin çapını giriniz: "))
+print("dairenin alanı:", 3.14 * r * r)
+
