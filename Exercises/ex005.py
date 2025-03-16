6d9cd83881612965e139389bb62269ec763fb020
miracarslan23 <aralan2000@icloud.com>
2025-03-16 23:15:04 +0300

    Update ex005.py

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:210543008
+# Your Name and Surname:mustafa miraç arslan
+name = input("please enter your name:" )
+surname = input("please enter your surname:" )
+print("hi",surname,name)
