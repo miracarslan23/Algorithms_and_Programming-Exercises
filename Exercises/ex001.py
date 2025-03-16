b1ffe80f8ccc9f5176c129e098846ff0515f51de
miracarslan23 <aralan2000@icloud.com>
2025-03-16 23:01:48 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,4 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:210543008
+# Your Name and Surname:musatafa miraç arslan
+print("Twinkle, twinkle, little star, \n  \tHow I wonder what you are! \n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n \t How I wonder what you are")
+
