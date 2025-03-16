5618558dbe9399667899c79fcc536d48e5a3192f
miracarslan23 <aralan2000@icloud.com>
2025-03-16 23:17:17 +0300

    Update ex006.py

--- Exercises/ex006.py
+++ Exercises/ex006.py
@@ -1,2 +1,15 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:210543008
+# Your Name and Surname:mustafa miraç arslan 
+a = [1,2,3,4,5,6,7,8,9,10]
+print("original:",a)
+print("reverse list",a[::-1])
+a.extend([11,12,13])
+print(a)
+print(len(a))
+a.pop(0)
+a.pop(-1)
+print(a)
+print("even numbers:",a[::2])
+
+
+
